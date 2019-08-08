ef00e0cd5091f695cb007949795610eab9f52f54
Anithasivakumar <37569380+Anithasivakumar@users.noreply.github.com>
2019-08-08 14:28:12 +0530

    Add files via upload

---
+++ weekend.py
@@ -0,0 +1,15 @@
+d=str(input(""))
+if d=="Monday":
+    print("no")
+elif d=="Tuesday":
+    print("no")
+elif d=="Wednesday":
+    print("no")
+elif d=="Thursday":
+    print("no")
+elif d=="Friday":
+    print("no")
+elif d=="Saturday":
+    print("yes")
+elif d=="Sunday":
+    print("yes")
